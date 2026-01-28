b295e6024b65c76605cd83e55fb1c77d29d607c7
Nook <pattarapon.kasco@gmail.com>
2026-01-28 14:01:37 +0700

    ไปก่อน

--- src/pages/proactive-monitor/index.ts
+++ src/pages/proactive-monitor/index.ts
@@ -3,4 +3,3 @@
 export { default as ProactiveMonitorCockpit } from "./ProactiveMonitorCockpit";
 export { default as OverallMonitoringTab } from "./OverallMonitoringTab";
 export { default as ProactiveMonitoringTab } from "./ProactiveMonitoringTab";
-export { default as ProactiveMonitoring } from "./ProactiveMonitoring";
